f07b7e5fc814cf3c75449e7832fa14c9e5f4550e
theduelistgirl <31298943+theduelistgirl@users.noreply.github.com>
2018-04-15 01:54:09 -0400

    near-nightly update commit
    Fleshing out the dynamic UI, still need to add code comments describing wth it does :P

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-$.(document).ready(function() {
-    $.("#add-button").click(function() {
-        "#add-button".append('<div id="rawr-xD" class="timer-box"></div>');
+var tNum = 2;
+
+$(document).ready(function(){
+    $("#add-button").click(function(){
+        if(tNum <= 3) {
+            $("#center-me").prepend(function(){
+                return '<div id="game' + tNum + '" class="box-timer" "game-timer"><div id="' + tNum + '" class="clock-face">00:00</div><button id="' + tNum + '" class="start-button clock-button"></button><button id="' + tNum + '" class="stop-button clock-button"></button><button id="' + tNum + '" class="rem-button clock-button"></button></div>';
+            });
+            tNum++;
+        }else if(tNum == 4){
+            $("#center-me").prepend(function(){
+                return '<div id="game' + tNum + '" class="box-timer" "game-timer"><div id="' + tNum + '" class="clock-face">00:00</div><button id="' + tNum + '" class="start-button clock-button"></button><button id="' + tNum + '" class="stop-button clock-button"></button><button id="' + tNum + '" class="rem-button clock-button"></button></div>';
+            });
+            $("#add-button").hide();
+        }
     });
 });
