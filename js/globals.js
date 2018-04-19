44e19fae8517311ac9ce10508b0001c442af2ec9
theduelistgirl <31298943+theduelistgirl@users.noreply.github.com>
2018-04-19 15:54:04 -0400

    In-progress commit
    To be pulled again later. Changes are heavily in progress.

--- js/globals.js
+++ js/globals.js
@@ -1,36 +1,57 @@
 //tracks timers currently created
+
 var tNum = 1;
 
 //screen dimensions
+
 var vportX = null;
 var vportY = null;
 
+// Convert milliseconds to format [ minutes : seconds ]
+// Code from StackOverflow users 'maerics' and 'Michael Simonitsch'
+
+function millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
 //pre-initialize our timers, because bad code practive is my favourite kind
+
 var coffiiCup1 = new Timer({
 //  onstop  : add code later because lazy
     tick    : 1,
     ontick  : function(ms) {
         console.log(ms + ' milliseconds left');
-        var sec = ms.toString().slice(0, -3);
-        if(ms.toString().length <= 5) {
-            var min = "00";
-        }
         var div = document.getElementById('1');
-        div.innerHTML = min + ':' + sec;
+        div.innerHTML = millisToMinutesAndSeconds(ms).toString();
     }
 });
+
 var coffiiCup2 = new Timer({
 //  onstop  : add code later because lazy
     tick    : 1,
-    ontick  : function(ms) { console.log(ms + ' milliseconds left') }
+    ontick  : function(ms) {
+        console.log(ms + ' ms timer(2) started');
+        var div = document.getElementById('2');
+        div.innerHTML = millisToMinutesAndSeconds(ms).toString();
+    }
 });
 var coffiiCup3 = new Timer({
 //  onstop  : add code later because lazy
     tick    : 1,
-    ontick  : function(ms) { console.log(ms + ' milliseconds left') }
+    ontick  : function(ms) {
+        console.log(ms + ' ms timer(3) started');
+        var div = document.getElementById('3');
+        div.innerHTML = millisToMinutesAndSeconds(ms).toString();
+    }
 });
 var coffiiCup4 = new Timer({
 //  onstop  : add code later because lazy
     tick    : 1,
-    ontick  : function(ms) { console.log(ms + ' milliseconds left') }
+    ontick  : function(ms) {
+        console.log(ms + ' mms timer(4) started');
+        var div = document.getElementById('4');
+        div.innerHTML = millisToMinutesAndSeconds(ms).toString();
+    }
 });
--- js/main-jquery.js
+++ js/main-jquery.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     $("#add-button").click(function(){
         $("#center-me").prepend(function(){
             tNum++;
-            return '<div id="game' + tNum + '" class="box-timer" "game-timer"><div id="' + tNum + '" class="clock-face">00:00</div><button id="' + tNum + '" class="start-button clock-button">Start</button><button id="' + tNum + '" class="stop-button clock-button">Stop/Pause</button><button id="' + tNum + '" class="rem-button clock-button">Close Me</button></div>';
-        });
+            return '<div id="game' + tNum + '" class="box-timer" "game-timer"><div id="' + tNum + '" class="clock-face">00:00</div><button id="start' + tNum + '" class="start-button clock-button">Start</button><button id="stop' + tNum + '" class="stop-button clock-button">Stop/Pause</button><button id="close' + tNum + '" class="rem-button clock-button">Close Me</button></div>';
+        }); 
         
         if(tNum <= 3) {
         }else if(tNum == 4){
@@ -13,38 +13,32 @@ $(document).ready(function(){
         }
 
     });
-    $(".start-button").click(function(){
-        switch(tNum) {
-            case 1:
-                coffiiCup1.start(120);
-                break;
-            case 2:
-                coffiiCup2.start(40);
-                break;
-            case 3:
-                coffiiCup3.start(40);
-                break;
-            case 4:
-                coffiiCup4.start(40);
-                break;
-        }
+    $("#start1").click(function(){
+        coffiiCup1.start(150);
     });
-    $(".stop-button").click(function(){
-        switch(tNum) {
-            case 1:
-                coffiiCup1.stop();
-                break;
-            case 2:
-                coffiiCup2.stop();
-                break;
-            case 3:
-                coffiiCup3.stop();
-                break;
-            case 4:
-                coffiiCup4.stop();
-                break;
-        }
+    $("#start2").click(function(){
+        coffiiCup2.start(150);
     });
+    $("#start3").click(function(){
+        coffiiCup3.start(150);
+    });
+    $("#start4").click(function(){
+        coffiiCup4.start(150);
+    });
+    $("#stop1").click(function(){
+        coffiiCup1.stop();
+    });
+    $("#stop2").click(function(){
+        coffiiCup2.stop();
+    });
+    $("#stop3").click(function(){
+        coffiiCup3.stop();
+    });
+    $("#stop4").click(function(){
+        coffiiCup4.stop();
+    });
+    //var div = document.getElementById(this.);
+    
     /* adjust timer(s) size on browser window resize
     $(window).resize(function(){                
         vportSet();
